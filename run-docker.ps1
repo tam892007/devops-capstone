a0eb68751d79585522528abe8434f6706da3a21a
Tam Lai <tam892007@gmail.com>
2023-04-14 02:37:11 +0800

    CircleCI: fix deploy jobs

--- run-docker.ps1
+++
@@ -1,6 +0,0 @@
-# Setup local HTTPS
-dotnet dev-certs https -ep "$env:USERPROFILE\.aspnet\https\aspnetapp.pfx"  -p "password"
-dotnet dev-certs https --trust
-
-# Run docker-compose
-docker-compose up
